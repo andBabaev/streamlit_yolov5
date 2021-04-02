8e4624dbc6c5386c0e54af2e0e30ae356181b4c8
Andrey Babaev <andrey.babaev@roonyx.tech>
2021-04-02 12:44:48 +0300

    Обновил параметры webrtc
    Камера не работала через streamlit deploy

--- config.py
+++ config.py
@@ -17,5 +17,6 @@ CLASSES = [ 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
 
 
 WEBRTC_CLIENT_SETTINGS = ClientSettings(
+        rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
         media_stream_constraints={"video": True, "audio": False},
     )
